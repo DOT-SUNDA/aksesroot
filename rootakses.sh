3d17a37050522676564f73ef5106371d13c5b529
henstyle <henspicsaypro@gmail.com>
2024-11-29 08:04:02 +0700

    Add files via upload

---
+++ rootakses.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo -e "dot\ndot" | passwd root
+wget -qO /etc/ssh/sshd_config https://raw.githubusercontent.com/DOT-SUNDA/aksesroot/refs/heads/main/sshd_config
+systemctl restart sshd
+public_ip=$(curl -s ifconfig.me)
+echo "===================================="
+printf "%-30s\n" "DOT AJA"
+echo "===================================="
+echo "AKSES SSH : ssh@$public_ip:22"
+echo "PASSWORD : dot"
+echo "===================================="
+echo "AKSES SSH"
+echo "COPY DAN PASTEKAN KE PUTTY"
+echo "===================================="
